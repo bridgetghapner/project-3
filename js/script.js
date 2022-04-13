9097ef2a638dabc11fea1ea1cc745dc6ddc48eb4
Bridget Hapner <bridgethapner@gmail.com>
2022-04-12 21:05:56 -0500

    changing map coords

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 function initMap() {
   // The location of Uluru
-  const uluru = { lat: -25.344, lng: 131.036 };
+  const uluru = { lat: 41.8781, lng: 87.6298 };
   // The map, centered at Uluru
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 4,
