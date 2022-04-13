6b9cca88097083109123595131ce9569a40d02de
Bridget Hapner <bridgethapner@gmail.com>
2022-04-12 21:13:26 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 function initMap() {
   const chicago = { lat: 41.8781, lng: -87.6298 };
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
+    zoom: 6,
     center: chicago,
   });
   const marker = new google.maps.Marker({
