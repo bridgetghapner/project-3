c0b146e6aa6a9d0443be5102bd3179f86b43d3d4
Bridget Hapner <bridgethapner@gmail.com>
2022-04-18 16:29:40 -0500

    added js link to head of cat info page

--- js/script.js
+++ js/script.js
@@ -40,12 +40,15 @@ function initMap() {
 
 var slide_index = 1;
 slidesDisplay(slide_index);
+
 function nextSlide(n) {
   slidesDisplay(slide_index += n);
 }
+
 function currentSlide(n) {
   slidesDisplay(slide_index = n);
 }
+
 function slidesDisplay(n) {
   var i;
   var slides = document.getElementsByClassName("showSlider");
