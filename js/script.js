1a1edb8ad7a870749541fb20f8a989185b5720f7
Bridget Hapner <bridgethapner@gmail.com>
2022-04-18 16:25:28 -0500

    trying to add slideshow to cat page

--- js/script.js
+++ js/script.js
@@ -33,4 +33,26 @@ function initMap() {
 
   chicagoPath.setMap(map);
 
-} 
+}
+
+
+// code for slideshow
+
+var slide_index = 1;
+slidesDisplay(slide_index);
+function nextSlide(n) {
+  slidesDisplay(slide_index += n);
+}
+function currentSlide(n) {
+  slidesDisplay(slide_index = n);
+}
+function slidesDisplay(n) {
+  var i;
+  var slides = document.getElementsByClassName("showSlider");
+  if (n > slides.length) { slide_index = 1 }
+  if (n < 1) { slide_index = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slide_index - 1].style.display = "block";
+}
